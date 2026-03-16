b29512b77d0893b97203c6f5902a278aa74d72b7
Vincent-Lin-UF <vincentlin1617@gmail.com>
2026-03-16 10:41:03 -0400

    added /api/chat endpoint and wired up router

--- backend/agent/main.py
+++ backend/agent/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from agent.api.routes import router
 
 app = FastAPI()
 
 # Change later -> right now it is allowing everything
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=[*],
-    allow_headers=[""],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
+app.include_router(router, prefix="/api")
+
 
 @app.get("/health")
 async def get_health():
-    return {"status" : "healthy"}
-
-
+    return {"status": "healthy"}
