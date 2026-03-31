53c2503001f649f82bad6a9c3080279c62d76dfb
Anthony Le <anthonyle@ufl.edu>
2026-03-31 15:51:38 -0400

    implemented components for bucket list, needs styling

--- frontend/src/app/bucket-list/bucket-list.ts
+++ frontend/src/app/bucket-list/bucket-list.ts
@@ -1,9 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+interface BucketItem {
+  name: string;
+  location: string;
+  tags: string;
+  image: string;
+}
+
 @Component({
-  selector: 'app-signup',
-  imports: [RouterLink],
+  selector: 'app-bucket-list',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './bucket-list.html',
 })
-export class BucketList {}
+export class BucketList {
+
+  bucketList: BucketItem[] = [
+    {
+      name: 'Visit Japan',
+      location: 'Tokyo',
+      tags: 'travel, culture',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfx451b6f6nSPYh7E2aN5Q24tyIxjKHXsqMg&s'
+    },
+        {
+      name: 'Visit Japan',
+      location: 'Tokyo',
+      tags: 'travel, culture',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfx451b6f6nSPYh7E2aN5Q24tyIxjKHXsqMg&s'
+    },
+    {
+      name: 'Skydiving',
+      location: 'California',
+      tags: 'adventure, high priority',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfx451b6f6nSPYh7E2aN5Q24tyIxjKHXsqMg&s'
+    },
+        {
+      name: 'Skydiving',
+      location: 'California',
+      tags: 'adventure, high priority',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfx451b6f6nSPYh7E2aN5Q24tyIxjKHXsqMg&s'
+    },
+        {
+      name: 'Skydiving',
+      location: 'California',
+      tags: 'adventure, high priority',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfx451b6f6nSPYh7E2aN5Q24tyIxjKHXsqMg&s'
+    },
+        {
+      name: 'Skydiving',
+      location: 'California',
+      tags: 'adventure, high priority',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfx451b6f6nSPYh7E2aN5Q24tyIxjKHXsqMg&s'
+    }
+    ,    {
+      name: 'Skydiving',
+      location: 'California',
+      tags: 'adventure, high priority',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfx451b6f6nSPYh7E2aN5Q24tyIxjKHXsqMg&s'
+    }
+  ];
+
+}
