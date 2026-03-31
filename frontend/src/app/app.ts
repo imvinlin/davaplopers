e1ce5a5aee9e3e62829b90ebe05c41dd0f96a156
Vincent-Lin-UF <vincentlin1617@gmail.com>
2026-03-31 14:52:21 -0400

    added app config and routing

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet],
+  template: '<router-outlet />',
 })
 export class AppComponent {}
