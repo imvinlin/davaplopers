0f654dcfd85b5e23939bbbfb185b27bea5b2794d
Vincent-Lin-UF <vincentlin1617@gmail.com>
2026-03-31 15:09:14 -0400

    added in google oauth

--- frontend/src/app/signup/signup.ts
+++ frontend/src/app/signup/signup.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare const google: any;
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink],
   templateUrl: './signup.html',
 })
-export class SignupComponent {}
+export class SignupComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    google.accounts.id.initialize({
+      client_id: '143526963027-1vb8npsrdgjdhg92goh91nekbs79dvib.apps.googleusercontent.com',
+      callback: () => this.router.navigate(['/chat']),
+    });
+    google.accounts.id.renderButton(
+      document.getElementById('google-btn'),
+      { theme: 'filled_black', size: 'large', text: 'continue_with' }
+    );
+  }
+}
