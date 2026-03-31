591ed2b4e8b12ea114c8d31f7c47e87870409902
Vincent-Lin-UF <vincentlin1617@gmail.com>
2026-03-31 14:53:02 -0400

    refactoring and moved signup component

---
+++ frontend/src/app/signup/signup.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  imports: [RouterLink],
+  templateUrl: './signup.html',
+})
+export class SignupComponent {}
