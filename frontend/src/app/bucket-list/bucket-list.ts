4d900371aa4f22487fd9475eb396ae66c6966aeb
Anthony Le <anthonyle@ufl.edu>
2026-03-31 15:01:33 -0400

    page setup

---
+++ frontend/src/app/bucket-list/bucket-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  imports: [RouterLink],
+  templateUrl: './bucket-list.html',
+})
+export class BucketList {}
