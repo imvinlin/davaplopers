6341dd13a242173a1255bd21b886f882d8d17452
Vincent-Lin-UF <vincentlin1617@gmail.com>
2026-04-21 10:21:40 -0400

    chore: point prod env at digitalocean droplet

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBase: 'https://f929-2605-ad80-10-2c11-49d4-9e70-12ab-2e0f.ngrok-free.app',
+  apiBase: 'https://104.248.121.216.sslip.io',
 };
